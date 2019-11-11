7b94fea795fe028e2c0b37601fca9de88976d033
Drew Seeholzer <drews21f7@gmail.com>
2019-11-11 15:45:30 -0700

    Made CoreDataStack and Stats+Convenience files

---
+++ timestables/Models/Stats+Convenience.swift
@@ -0,0 +1,21 @@
+//
+//  Stats+Convenience.swift
+//  timestables
+//
+//  Created by Drew Seeholzer on 11/11/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Stats {
+    convenience init(attempts: Int64, averageScore: Double, highestScore: Double, lastScore: Double, previousScore: Double, context: NSManagedObjectContext = CoreDataStack.managedObjectContext) {
+        self.init(context: context)
+        self.attempts = attempts
+        self.averageScore = averageScore
+        self.highestScore = highestScore
+        self.lastScore = lastScore
+        self.previousScore = previousScore
+    }
+}
