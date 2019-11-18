6fa70145aebd34d7dd2e85fb10da19ac84c6c462
Drew Seeholzer <drews21f7@gmail.com>
2019-11-18 13:43:50 -0700

    Finished filling out statsController, changed scoreArray in model to cumulative score.

--- timestables/Model Controller/StatsController.swift
+++ timestables/Model Controller/StatsController.swift
@@ -39,15 +39,15 @@ class StatsController {
     
     // Initializes stats and stats array on the first practice
     func initializeStats() {
-        let statsGlobal = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, scoresArray:[])
-        let statsTwos = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, scoresArray:[])
-        let statsThrees = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, scoresArray:[])
-        let statsFours = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, scoresArray:[])
-        let statsFives = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, scoresArray:[])
-        let statsSixes = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, scoresArray:[])
-        let statsSevens = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, scoresArray:[])
-        let statsEights = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, scoresArray:[])
-        let statsNines = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, scoresArray:[])
+        let statsGlobal = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, cumulativeScore: 0)
+        let statsTwos = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, cumulativeScore: 0)
+        let statsThrees = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, cumulativeScore: 0)
+        let statsFours = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, cumulativeScore: 0)
+        let statsFives = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, cumulativeScore: 0)
+        let statsSixes = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, cumulativeScore: 0)
+        let statsSevens = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, cumulativeScore: 0)
+        let statsEights = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, cumulativeScore: 0)
+        let statsNines = Stats(attempts: 0, averageScore: 0, highestScore: 0, lastScore: 0, currentScore: 0, cumulativeScore: 0)
         stats.append(statsGlobal)
         stats.append(statsTwos)
         stats.append(statsThrees)
@@ -59,15 +59,35 @@ class StatsController {
         stats.append(statsNines)
     }
     
-    // Function to be called each time a practice run is completed. Updates all stats attributes except average score. That is to be calculated when the stats view is loaded.
+    // Function to be called each time a practice run is completed. Updates all stats attributes. It also updates the global lastScore and currentScore.
+    
     func updateStatsNonGlobal(stats: Stats, score: Double) {
+        let globalStats = StatsController.sharedInstance.stats[0]
         stats.attempts += 1
+        stats.cumulativeScore += score
         if score > stats.highestScore {
             stats.highestScore = score
         }
+        globalStats.lastScore = globalStats.currentScore
+        globalStats.currentScore = score
         stats.lastScore = stats.currentScore
         stats.currentScore = score
-        stats.scoresArray?.append(score)
+        stats.averageScore = stats.cumulativeScore / stats.attempts
+        saveToPersistentStore()
+    }
+    
+    // Function to be called whenever the stats view is brought up. Adds all the stats together for a global count across all times tables practices.
+    
+    func updateStatsGlobal(globalStats: Stats) {
+        let statsArray = StatsController.sharedInstance.stats
+        for stats in statsArray where stats != statsArray[0] {
+            globalStats.attempts += stats.attempts
+            if stats.highestScore > globalStats.highestScore {
+                globalStats.highestScore = stats.highestScore
+            }
+            globalStats.cumulativeScore += stats.cumulativeScore
+        }
+        globalStats.averageScore = globalStats.cumulativeScore / globalStats.attempts
         saveToPersistentStore()
     }
     
